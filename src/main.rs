a16b098841eaac3e5a95b943370dfa6fe423b587
Igor Laborie <ilaborie@gmail.com>
2015-12-23 22:10:59 +0100

    update1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
 use std::fs;
+use std::fs::DirEntry;
 use std::path::Path;
 use std::io;
+use std::result;
 
 // Main
 fn main() {
@@ -11,20 +13,37 @@ fn main() {
     // Current dir
     let dir = env::current_dir().unwrap();
     println!("The current directory is {:?}", dir);
-    let result = process_dir(dir.as_path());
+
+    // Process
+    let result = process_dirs(dir.as_path());
+    // FIXME do not use format! for OK case
     let message = match result {
-        Result::Ok(true) => format!("[OK] with update(s)"),
-        Result::Ok(false) => format!("[OK] already synchronised"),
-        Result::Err(err) => format!("[ERR] Oops! {}", err),
+        Ok(true) => format!("[OK] with update(s)"),
+        Ok(false) => format!("[OK] already synchronised"),
+        Err(err) => format!("[ERR] Oops! {}", err),
     };
+    // FIXME chain call
     println!("{:?}", message);
 }
 
-// Process the root dir
-fn process_dir(dir: &Path) -> io::Result<bool> {
-    for entry in try!(fs::read_dir(dir)) {
-        let dir = try!(entry);
-        println!("{:?}", dir.path());
+// Process the root dir (flat)
+fn process_dirs(parent_dir: &Path) -> io::Result<bool> {
+    let mut res = false;
+    for entry in try!(fs::read_dir(parent_dir)) {
+        let child: DirEntry = try!(entry);
+        let path_buf = child.path();
+        let path = path_buf.as_path();
+        res = process_dir(&path) || res;
     }
-    Ok(true)
+    Ok(res)
 }
+
+fn process_dir(dir: &Path) -> bool {
+    println!("{:?}", dir);
+    // println!("{:?}", dir.path().as_path());
+    return true;
+}
+
+// fn is_git_repository(dir: &Path) -> bool {
+//   unimplemented!()
+// }
