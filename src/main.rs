0d6e4682fdf1bc431e6f75926ff92c3abfa7228d
Igor Laborie <ilaborie@gmail.com>
2015-12-24 16:43:29 +0100

    git fetch

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 extern crate getopts;
 extern crate git2;
 use getopts::Options;
-use std::env;
-use std::fs;
-use std::fs::DirEntry;
+use std::{env, fs, io};
+// use std::fs::DirEntry;
 use std::path::Path;
-use std::io;
 use std::error::Error;
 use git2::Repository;
 use git2::string_array::StringArray;
@@ -35,7 +33,7 @@ fn main() {
 
     // let dir = env::current_dir(input_dir);
     let main_dir = Path::new(&input_dir);
-    println!("Git Sync {:?}", main_dir.to_str());
+    println!("Git Sync {}", main_dir.to_str().unwrap());
 
     // Process
     let result = process_dirs(&main_dir);
@@ -66,8 +64,8 @@ fn process_dirs(parent_dir: &Path) -> io::Result<bool> {
             if dir_result.is_ok() {
                 res = dir_result.unwrap() || res;
             } else {
-                let gitErr = dir_result.err().unwrap();
-                return Err(io::Error::new(io::ErrorKind::Other, gitErr));
+                let git_err = dir_result.err().unwrap();
+                return Err(io::Error::new(io::ErrorKind::Other, git_err));
             }
         }
     }
@@ -85,19 +83,56 @@ fn process_dir(dir: &Path) -> Result<bool, git2::Error> {
 }
 
 fn process_repository(repo: Repository) -> Result<bool, git2::Error> {
-    println!("Detect Git repository {:?}", repo.path());
-    // Remotes
+    println!("Detected git repository {:?}", repo.path());
+    let mut res = false;
     let remotes: StringArray = try!(repo.remotes());
     if remotes.len() == 0 {
         println!("  [SKIP] no remotes");
     } else {
         for remote in remotes.iter() {
-            println!("  Sync {}", remote.unwrap());
+            if remote.is_some() {
+                let result_remote = process_repository_remote(&repo, remote.unwrap());
+                if result_remote.is_ok() {
+                    res = result_remote.unwrap() || res;
+                } else {
+                    println!("  [WARN] unable to sync {}", result_remote.unwrap_err());
+                }
+            }
         }
     }
-    Ok(true) // FIXME
+    Ok(res) // FIXME
 }
 
-fn process_repository_remote(repo: Repository, path: &str) -> Result<bool, git2::Error> {
+fn process_repository_remote(repo: &Repository, str_remote: &str) -> Result<bool, git2::Error> {
+    // Fetch
+    let mut remote = try!(repo.find_remote(str_remote));
+
+    // Extract refspecs
+    let mut vec_refspecs = Vec::new();
+    for refspec in remote.refspecs() {
+        let rs = refspec.str();
+        if rs.is_some() {
+            let rfspc = rs.unwrap().to_owned().clone();
+            vec_refspecs.push(rfspc);
+        }
+    }
+    let refspecs: Vec<&str> = vec_refspecs.iter().map(|s| s.as_ref()).collect();
+    println!("  Sync {} ({:?}) {:?}",
+             str_remote,
+             remote.url().unwrap(),
+             vec_refspecs);
+    let msg = format!("git-sync {}", str_remote).to_owned();
+    let reflog_msg: Option<&str> = Some(&*msg);
+
+    // let mut options = git2::FetchOptions::new();
+    // options.download_tags(git2::AutotagOption::All);
+    try!(remote.fetch(&refspecs[..], None /* Some(&mut options) */, reflog_msg));
+    // remote.fetch(&[refspecs], None, None);
+
+    // Branches
+    let branches = vec!["origin", "develop"];
+    for branch in branches.iter() {
+        println!("   merge? branch {}", branch);
+    }
     Ok(true) // FIXME
 }
